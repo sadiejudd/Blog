39b34d10d75f006021c5a60ac106550596b3a6ab
sadiejudd <sadiejudd@mail.weber.edu>
2026-02-12 19:38:30 -0700

    added displays the hobbies in the view

--- PersonalBlog/PortfolioDatabase/views.py
+++ PersonalBlog/PortfolioDatabase/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Hobby
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<strong>Welcome!</strong>")
+    return HttpResponse("""<strong>Welcome!</strong> 
+    My name is Sadie Judd. I am a Computer Science student at Weber State University.
+    I spend a lot of time studying and working, but I also like spending time with friends and family,
+    playing with my dog, and spening time outdoors.""")
+   
 
 def hobbies(request):
-    return
+    hobbies = Hobby.objects.all()
+    output = ""
+    for hobby in hobbies:
+        output+= f"{hobby.hobby_name}: {hobby.hobby_desc}\n"
+    return HttpResponse(output)
+
+    
+
 
 def portfolio(request):
     return
